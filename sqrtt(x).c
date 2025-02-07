44226eeb4f9762f5e6437172b29f71d0aa9b2654
Mohanavelayyavu <168104003+Mohanavelayyavu@users.noreply.github.com>
2025-02-07 16:57:53 +0530

    Create sqrtt(x).c

---
+++ sqrtt(x).c
@@ -0,0 +1,17 @@
+int mySqrt(int x) 
+{
+    if(x==0)
+    {
+        return 0;
+    }
+    int i=1;
+    while(i<=(x/i))
+    {
+        if((x/i)==i)
+        {
+            return i;
+        }
+        i++;
+    }
+    return i-1;
+}
