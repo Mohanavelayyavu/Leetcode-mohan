31e22f3f8e1884e48d3e0f4b4f9a00f00fc58bab
Mohanavelayyavu <168104003+Mohanavelayyavu@users.noreply.github.com>
2025-03-05 07:52:53 +0530

    Create 2535.java

---
+++ Java/2535.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int elementsum = 0, digitsum = 0;
+       for(int num:nums){
+           elementsum+=num;
+           
+        
+        int temp= num;
+        while(temp>0){
+            digitsum+=temp%10;
+            
+        temp/=10;
+            }
+           }
+        return Math.abs(elementsum-digitsum);
+        
+    }
+}
